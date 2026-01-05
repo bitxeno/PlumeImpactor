27c7b4ccb961d9c8ad630c7a8f04a1a9cacb9788
bitxeno <137328844+bitxeno@users.noreply.github.com>
2026-01-05 11:42:50 +0800

    fix: ensure crypto has  default provider

--- apps/plumesign/src/main.rs
+++ apps/plumesign/src/main.rs
@@ -5,13 +5,10 @@ use commands::{Cli, Commands};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let default_level = if cfg!(debug_assertions) {
-        "debug"
-    } else {
-        "info"
-    };
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(default_level))
-        .init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    _ = rustls::crypto::ring::default_provider()
+        .install_default()
+        .unwrap();
     let cli = Cli::parse();
 
     match cli.command {
