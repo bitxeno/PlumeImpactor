785fcfcc177b2d42ce34558b89c71f7cabb5ae49
bitxeno <137328844+bitxeno@users.noreply.github.com>
2026-01-05 11:32:28 +0800

    feat: auto download anisette deps

--- crates/core/src/auth/anisette_data.rs
+++ crates/core/src/auth/anisette_data.rs
@@ -1,6 +1,18 @@
 use std::collections::HashMap;
 use std::time::SystemTime;
 
+#[cfg(target_os = "linux")]
+use std::fs::{self, File};
+#[cfg(target_os = "linux")]
+use std::io::{self, Cursor, copy};
+
+#[cfg(target_os = "linux")]
+use reqwest::Client;
+#[cfg(target_os = "linux")]
+use zip::ZipArchive;
+
+#[cfg(target_os = "linux")]
+use omnisette::AnisetteError;
 use omnisette::{AnisetteConfiguration, AnisetteHeaders};
 
 use crate::Error;
@@ -14,6 +26,9 @@ pub struct AnisetteData {
 
 impl AnisetteData {
     pub async fn new(config: AnisetteConfiguration) -> Result<Self, Error> {
+        #[cfg(target_os = "linux")]
+        Self::check_and_download_deps_libs(&config.configuration_path()).await?;
+
         let mut b = AnisetteHeaders::get_anisette_headers_provider(config.clone())?;
         let base_headers = b.provider.get_authentication_headers().await?;
 
@@ -36,6 +51,76 @@ impl AnisetteData {
 
     pub async fn refresh(&self) -> Result<Self, crate::Error> {
         Self::new(self.config.clone()).await
+    }
+
+    #[cfg(target_os = "linux")]
+    async fn check_and_download_deps_libs(
+        configuration_path: &std::path::Path,
+    ) -> Result<(), AnisetteError> {
+        #[cfg(target_arch = "x86_64")]
+        let arch = "x86_64";
+        #[cfg(target_arch = "x86")]
+        let arch = "x86";
+        #[cfg(target_arch = "aarch64")]
+        let arch = "arm64-v8a";
+        #[cfg(target_arch = "arm")]
+        let arch = "armeabi-v7a";
+        #[cfg(not(any(
+            target_arch = "aarch64",
+            target_arch = "x86_64",
+            target_arch = "x86",
+            target_arch = "arm"
+        )))]
+        return Ok(());
+
+        let lib_path = configuration_path.join("lib").join(arch);
+        let libs = ["libstoreservicescore.so", "libCoreADI.so"];
+        let missing = libs.iter().any(|lib| !lib_path.join(lib).exists());
+
+        if !missing {
+            return Ok(());
+        }
+
+        if !lib_path.exists() {
+            fs::create_dir_all(&lib_path)?;
+        }
+
+        log::info!("Downloading Apple Music APK...");
+        let client = Client::builder().build()?;
+
+        let response = client
+            .get("https://apps.mzstatic.com/content/android-apple-music-apk/applemusic.apk")
+            .send()
+            .await?;
+
+        let content = response.bytes().await?;
+        let reader = Cursor::new(content);
+
+        let mut archive =
+            ZipArchive::new(reader).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        let apk_arch_path = format!("lib/{}/", arch);
+
+        for i in 0..archive.len() {
+            let mut file = archive
+                .by_index(i)
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            let name = file.name().to_string();
+
+            if name.starts_with(&apk_arch_path) && name.ends_with(".so") {
+                let file_name = std::path::Path::new(&name).file_name().unwrap();
+                let file_name_str = file_name.to_str().unwrap();
+
+                if libs.contains(&file_name_str) {
+                    let dest_path = lib_path.join(file_name);
+                    let mut dest_file = File::create(&dest_path)?;
+                    copy(&mut file, &mut dest_file)?;
+                }
+            }
+        }
+
+        log::info!("Anisette dependency extracted to: {}", lib_path.display());
+        Ok(())
     }
 
     pub fn generate_headers(
